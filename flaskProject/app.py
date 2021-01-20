96a02a07a00f8608ae82286e7f44ebf7387f9207
IlanitRozenberg <73186916+IlanitRozenberg@users.noreply.github.com>
2021-01-20 21:50:59 +0200

    Add files via upload

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, redirect, url_for, render_template, request, session
-from assignment10.app import assignment10
+from flask import Flask, redirect, url_for, render_template, request, session, make_response
+from assignment10.app import assignment10, interact_db
+import json
 
 app = Flask(__name__)
 app.secret_key = '123'
@@ -47,7 +48,32 @@ def assignment9_func():
     search = ''
     if 'search' in request.args:
         search = request.args['search']
-    return render_template('assignment9.html', search_term=search, username=username,)
+    return render_template('assignment9.html', search_term=search, username=username, )
+
+
+@app.route("/assignment11/users")
+@app.route("/assignment11/users/")
+def assignment11_func():
+    query = "SELECT * FROM users"
+    output = interact_db(query, 'fetch')
+    for x in range(len(output)):
+        temp = output[x]
+        output[x] = {"id": temp[0], "full name": temp[2], "email": temp[1]}
+    return make_response({"USERS": output}, 200)
+
+
+@app.route("/assignment11/users/selected")
+@app.route("/assignment11/users/selected/")
+@app.route("/assignment11/users/selected/<int:uid>")
+def assignment11_func2(uid=1):
+    query = "SELECT * FROM users WHERE id=%s" % uid
+    output = interact_db(query, 'fetch')
+    for x in range(len(output)):
+        temp = output[x]
+        output[x] = {"id": temp[0], "full name": temp[2], "email": temp[1]}
+    if len(output) == 0:
+        return make_response({"ERROR": "INVALID ID"}, 200)
+    return make_response(output[0], 200)
 
 
 if __name__ == '__main__':
